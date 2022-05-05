c990ab7d61aac10e94ed5c51e4bd371264414697
nurlanoverkin <beni.nurlanov@gmail.com>
2022-05-05 19:21:48 +0600

    done 30 homework

--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# class ToMeet(models.Model):
+#         persone = models.CharField(max_length=100)
+#         phone_number = models.CharField(max_length=12)
+#         date_of_meeting = models.DateField(auto_now_add=True)
+#         comment = models.CharField(max_length=12)
+#         is_closed = models.BooleanField(default=False)
+#         is_favorite = models.BooleanField(default=False)
+class ToMeet(models.Model):
+        persone = models.CharField(max_length=100)
+        phone_number = models.CharField(max_length=12)
+        date_of_meeting = models.DateField(auto_now_add=True)
+        comment = models.CharField(max_length=100)
+        is_closed = models.BooleanField(default=False)
+        is_favorite = models.BooleanField(default=False)
